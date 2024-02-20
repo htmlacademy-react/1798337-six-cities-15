fecf29cb23e3caf40de21f08fe7d32a6543e43cf
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2024-02-20 19:24:05 +0300

    добавляет компоненты app, index, mainpage

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import MainPage from '../../pages/main-page';
+
+// type AppScreenProps = {
+//   errorsCount: number;
+// }
+
+function App(): JSX.Element {
+  return (
+    <MainPage />
+    // <WelcomeScreen errorsCount={errorsCount} />
+  );
+}
+
+export default App;
