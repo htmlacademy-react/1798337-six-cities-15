fc7535b93cfb4d179b68752d341d6d237e8ec99d
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2024-03-12 17:29:40 +0300

    добавляет обработчик событий для карточек

--- src/components/offer-card.tsx
+++ src/components/offer-card.tsx
@@ -1,49 +1,69 @@
+import { Link } from 'react-router-dom';
 import { OfferType } from '../mock/offers-mock';
+import { AppRoute } from './const';
 
-function OfferCard({offer}:{offer: OfferType}): JSX.Element {
+type OfferCardPropsType = {
+  offer:OfferType;
+  handleHover: (offer?:OfferType) => void;
+}
+
+function OfferCard({offer, handleHover}:OfferCardPropsType): JSX.Element {
 
   const {id, title, type, price, isFavorite, isPremium, rating} = offer;
 
   const classFavoriteButton = isFavorite ? 'place-card__bookmark-button--active' : '';
   const ratingInProsent = `${rating * 20}%`;
 
+  const handleMouseOn = () => {
+    handleHover(offer);
+  };
+
+  const handleMouseOff = () => {
+    handleHover();
+  };
+
   return (
-    <article className="cities__card place-card" data-id={id}>
-      {isPremium ?
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
-        : ''}
-      <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
+    <Link to={`${AppRoute.Offer}/${offer.id}`}>
+      <article
+        className="cities__card place-card"
+        data-id={id}
+        onMouseEnter={handleMouseOn}
+        onMouseLeave={handleMouseOff}
+      >
+        {isPremium ?
+          <div className="place-card__mark">
+            <span>Premium</span>
+          </div>
+          : ''}
+        <div className="cities__image-wrapper place-card__image-wrapper">
           <img className="place-card__image" src={''} width="260" height="200" alt="Place image" />
-        </a>
-      </div>
-      <div className="place-card__info">
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{price}</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
-          </div>
-          <button className={`place-card__bookmark-button ${classFavoriteButton} button`}type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref={'#icon-bookmark'}></use>
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{ width: ratingInProsent}}></span>
-            <span className="visually-hidden">Rating</span>
+        <div className="place-card__info">
+          <div className="place-card__price-wrapper">
+            <div className="place-card__price">
+              <b className="place-card__price-value">&euro;{price}</b>
+              <span className="place-card__price-text">&#47;&nbsp;night</span>
+            </div>
+            <button className={`place-card__bookmark-button ${classFavoriteButton} button`}type="button">
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref={'#icon-bookmark'}></use>
+              </svg>
+              <span className="visually-hidden">To bookmarks</span>
+            </button>
           </div>
+          <div className="place-card__rating rating">
+            <div className="place-card__stars rating__stars">
+              <span style={{ width: ratingInProsent}}></span>
+              <span className="visually-hidden">Rating</span>
+            </div>
+          </div>
+          <h2 className="place-card__name">
+            <a href="#">{title}</a>
+          </h2>
+          <p className="place-card__type">Apartment{type.charAt(0)}</p>
         </div>
-        <h2 className="place-card__name">
-          <a href="#">{title}</a>
-        </h2>
-        <p className="place-card__type">Apartment{type.charAt(0)}</p>
-      </div>
-    </article>
+      </article>
+    </Link>
   );
 }
 
--- src/components/offers-card-list.tsx
+++ src/components/offers-card-list.tsx
@@ -1,19 +1,23 @@
 import { OfferType } from '../mock/offers-mock';
-
-import PlaceCard from './offer-card';
+import { Nullable } from 'vitest';
+import { useEffect, useState } from 'react';
+import OfferCard from './offer-card';
 
 function OffersCardList(props: { offers: OfferType[] }): JSX.Element {
   const [activeOffer, setActiveOffer] = useState<Nullable<OfferType>>(null);
   const handleOfferHover = (offer?: OfferType) => {
     setActiveOffer(offer || null);
-  }
+  };
+
+  useEffect(() => console.log(activeOffer));
 
   return (
     <div className="cities__places-list places__list tabs__content">
-      {props.offers.map((offer: OfferType) => (
-        <PlaceCard
+      {props.offers.map((offer) => (
+        <OfferCard
           key = {offer.id}
           offer = {offer}
+          handleHover = {handleOfferHover}
         />
       ))}
     </div>
